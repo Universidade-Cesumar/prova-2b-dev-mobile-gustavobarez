e24ea162de197d055ee2f0d73040d57d238bd918
gustavobarez <gbarez44@gmail.com>
2026-06-11 19:38:03 -0300

    feat: adiciona constants.js com URL base do MockAPI

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+// ATENÇÃO: Substitua pela URL do seu MockAPI!
+export const MOCKAPI_BASE_URL = 'https://SEU_ID_AQUI.mockapi.io';
+
+export const ENDPOINTS = {
+  materiais: `${MOCKAPI_BASE_URL}/materiais`,
+};
